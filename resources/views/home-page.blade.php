2003a1427b156a17424298b6d90a3959dc74f880
prisscillass <priscillasudiyantoro@gmail.com>
2025-06-04 13:36:38 +0700

    update some layout for homepage

--- resources/views/home-page.blade.php
+++ resources/views/home-page.blade.php
@@ -3,4 +3,22 @@
     <div class="w-full max-w-screen-xl m-auto">
         <x-carousel />
     </div>
+    
+    <div class="max-w-screen-xl m-auto mt-16 flex flex-col justify-center items-center gap-4">
+        <p class="text-primary-2 text-2xl font-semibold">CATEGORIES</p>
+        <div class="border border-primary-2 w-full h-20 rounded-2xl">
+            
+        </div>
+    </div>
+
+    <div class="max-w-screen-xl m-auto mt-16 flex flex-col justify-center items-center gap-4">
+        <p class="text-primary-2 text-2xl font-semibold">OUR PRODUCTS</p>
+
+        {{-- cart items --}}
+    </div>
+
+    <div>
+        <p class="text-primary-2">What Are We?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores dicta doloremque earum possimus consequuntur laborum, eum odit suscipit beatae, error, fugit a nostrum molestias eius sapiente veniam incidunt odio repudiandae.</p>
+    </div>
 </x-layout>
