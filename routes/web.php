c5a417fa79009b8f8f2997310ace3848f2a2dca8
divdiraaa <diraaadumpss@gmail.com>
2025-06-09 01:19:09 +0700

    update routes and controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/product/{id}', [ProductController::class, 'show'])->name('product.detail');
