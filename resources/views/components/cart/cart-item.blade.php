c76680478412878fa983ebfeb78d81d47b041155
divdiraaa <diraaadumpss@gmail.com>
2025-06-09 20:04:27 +0700

    create cart page with controller, model and routes

---
+++ resources/views/components/cart/cart-item.blade.php
@@ -0,0 +1,18 @@
+<div class="flex gap-4 border-b pb-4">
+    {{-- Gambar Produk --}}
+    <div class="w-24 h-24 rounded overflow-hidden shadow-sm">
+        {{ $image }}
+    </div>
+
+    {{-- Detail Produk --}}
+    <div class="flex-1">
+        {{ $details }}
+    </div>
+
+    {{-- Tombol Delete --}}
+    <button class="text-gray-600 hover:text-red-700">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H3.5a.5.5 0 000 1H4l.401 9.593A2 2 0 006.4 17h7.2a2 2 0 001.999-1.907L16 5h.5a.5.5 0 000-1H15V3a1 1 0 00-1-1H6zm2 5a.5.5 0 011 0v6a.5.5 0 01-1 0V7zm3 .5a.5.5 0 00-1 0v6a.5.5 0 001 0v-6z" clip-rule="evenodd" />
+        </svg>
+    </button>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,11 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CartController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/product/{id}', [ProductController::class, 'show'])->name('product.detail');
+
+Route::get('/cart', [CartController::class, 'show'])->name('cart.show');
